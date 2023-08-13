4927934ce3f96502b76a293233949fed38dc1e64
huyenma <huyentay2112@gmail.com>
2023-08-13 16:42:06 +0700

    add unit command

--- BaseHA.Domain/gen.bat
+++ BaseHA.Domain/gen.bat
@@ -1,3 +1,3 @@
 rmdir Entity /s /q
 rmdir Contexts /s /q
-dotnet ef dbcontext scaffold "Data Source=desktop-itlr9t6;Initial Catalog=WarehouseManagement;Integrated Security=True" Microsoft.EntityFrameworkCore.SqlServer -o Entity -c WareHouseContext -f --context-dir Contexts
+dotnet ef dbcontext scaffold "Data Source=ADMIN;Initial Catalog=WarehouseManagement;Integrated Security=True" Microsoft.EntityFrameworkCore.SqlServer -o Entity -c WareHouseContext -f --context-dir Contexts
