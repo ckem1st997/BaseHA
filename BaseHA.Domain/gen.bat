fd8658694af3ca07a094ce7efc5074e2189d14a4
NGUYEN KHA HOP <1721050681@student.humg.edu.vn>
2023-09-21 11:30:30 +0700

    update

--- BaseHA.Domain/gen.bat
+++ BaseHA.Domain/gen.bat
@@ -1,4 +1,4 @@
 rmdir Entity /s /q
 rmdir Contexts /s /q
-dotnet ef dbcontext scaffold "Data Source=ADMIN;Initial Catalog=WarehouseManagement;Integrated Security=True" Microsoft.EntityFrameworkCore.SqlServer -o Entity -c WareHouseContext -f --context-dir Contexts
+dotnet ef dbcontext scaffold "Data Source=it\mssqlserver01;Initial Catalog=WarehouseManagement;Integrated Security=True" Microsoft.EntityFrameworkCore.SqlServer -o Entity -c WareHouseContext -f --context-dir Contexts
 
